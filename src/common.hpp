6c2ec182c4ee70e79d1cb31f753a15e9c71e1342
Compro-Prasad <comproprasad@gmail.com>
2017-12-16 23:00:41 +0530

    Now custom columns can be added in displayCSV

--- src/common.hpp
+++ src/common.hpp
@@ -3,12 +3,41 @@
 
 #include <vector>
 #include <ostream>
+#include <string>
 
 template <typename T>
 std::ostream& operator<<(std::ostream &out, std::vector<T> v) {
     for (auto &i : v)
         out << i;
     return out;
+}
+
+std::vector<std::string> split(std::string s, char c) {
+    using namespace std;
+    string temp = "";
+    vector<string> result;
+    for (auto &i : s) {
+        if (i != c) {
+            temp.push_back(i);
+        } else {
+            result.push_back(move(temp));
+        }
+    }
+    result.push_back(move(temp));
+    return result;
+}
+
+int toInt(std::string s) {
+    int result = 0;
+    for (auto &i : s) {
+        if (i != '\0') {
+            if (isdigit(i))
+                result = result * 10 + i - 48;
+            else
+                throw std::string("toInt Error: Not a digit\n");
+        }
+    }
+    return result;
 }
 
 #endif
--- src/displayCSV.cpp
+++ src/displayCSV.cpp
@@ -1,15 +1,43 @@
+#include <unistd.h>
 #include "./csv.hpp"
 #include "./common.hpp"
 
 int main(int argc, char **argv) {
-    try {
-        CSV csv(argv[1], ';', '\\');
-        while (true) {
-            vector<string> row = std::move(csv.getNextRow());
-            cout << row << endl;
+    vector<int> columns;
+    while (true) {
+        char option = getopt(argc, argv, "c:f:");
+        if (option == -1)
+            break;
+        switch (option) {
+        case 'c':
+            columns.clear();
+            for (auto &i : split(optarg, ','))
+                columns.push_back(toInt(i));
+            break;
+        case 'f':
+            try {
+                CSV csv(optarg, ';', '\\');
+                if (columns.size() != 0) {
+                    while (true) {
+                        vector<string> row = move(csv.getNextRow());
+                        for (int i = 0, k = 0; i < row.size(); ++i) {
+                            if (i + 1 == columns[k]) {
+                                cout << row[i] << ", ";
+                                ++k;
+                            }
+                        }
+                        cout << endl;
+                    }
+                } else {
+                    while (true) {
+                        vector<string> row = move(csv.getNextRow());
+                        cout << row << endl;
+                    }
+                }
+            } catch (const string &s) {
+                cout << s << endl;
+            }
         }
-    } catch (const string &s) {
-        cout << s << endl;
     }
     return 0;
 }
